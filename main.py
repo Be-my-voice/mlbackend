ced8eaf5e51cee4f1c7974e0442251c922d697f3
Neethamadhu Madurasinghe <neethamadhu.ma@gmail.com>
2023-10-29 12:57:16 +0530

    Add initial skeleton

---
+++ main.py
@@ -0,0 +1,25 @@
+from typing import Union
+from fastapi import FastAPI
+from pydantic import BaseModel 
+
+app = FastAPI()
+
+
+class Base64Video(BaseModel):
+    data: str
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.post("/predict/video")
+def predict_from_video(base64Video: Base64Video):
+    print(base64Video.data)
+    return base64Video
+
