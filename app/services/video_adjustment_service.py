ed10c24321f7f30fbe2852da063e42c17906e7c5
Neethamadhu Madurasinghe <neethamadhu.ma@gmail.com>
2023-10-31 11:34:12 +0530

    Fix video scalling function

--- app/services/video_adjustment_service.py
+++ app/services/video_adjustment_service.py
@@ -2,6 +2,8 @@ import cv2
 import os
 import subprocess
 
+from app.utils.utils import generate_random_name, remove_video_file
+
 def check_video_resolution(filename):
     try:
         cap = cv2.VideoCapture(filename)
@@ -13,15 +15,22 @@ def check_video_resolution(filename):
 
         if width == int(os.getenv("VIDEO_WIDTH")) and height == int(os.getenv("VIDEO_HEIGHT")):
             return 0
-        else:
+        elif width == height:
             return 1
+        else:
+            return 2
 
     except Exception as e:
         print(f"An error occurred: {str(e)}")
-        return 2
+        return 3
 
 
 def convert_to_720x720(file_path):
+    directory, filename = os.path.split(file_path)
+    new_file_name = generate_random_name(10) + '.mp4'
+
+    new_file_path = os.path.join(directory, new_file_name)
+
     try:
         # Use FFmpeg to convert the video to 720x720 resolution and save it with the same filename
         command = [
@@ -30,12 +39,15 @@ def convert_to_720x720(file_path):
             '-vf', 'scale=720:720',      # Set the resolution to 720x720
             '-c:a', 'copy',              # Copy audio codec
             '-y',                        # Overwrite the input file
-            file_path                    # Output video file with the same filename
+            new_file_path                    # Output video file with the same filename
         ]
 
         subprocess.run(command, check=True)
         print(f"Video has been converted to 720x720 and saved as {file_path}")
 
+        return new_file_path
+
     except subprocess.CalledProcessError as e:
         print(f"An error occurred: {str(e)}")
+        return False
 
